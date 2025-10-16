501ced5ed2addd12eaf93d62455f28fa21c3d6e4
jsiqstr <js.iqstr@gmail.com>
2025-10-16 11:44:08 +0900

    feat : 파이프 충돌/점수 처리 추가

--- Assets/Scripts/PipeMovement.cs
+++ Assets/Scripts/PipeMovement.cs
@@ -1,37 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PipeMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 2.0f;
-    //이동 속도. 다가오는 플레이어와 조절하는 과정 필요할 듯...
-    
+    //변경 처리 가능한 기본 이동 속도
+    //유니티에서 플레이어와 확인 필요
+
+    private float Speed;
+    //실제 적용될 속도
+
+    public int Score;   //점수 (통과한 파이프 수)
+
+    //점수별 속도 조절 변수/함수 필요
+
     void Update()
     {
-        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * Speed * Time.deltaTime);
     }
 
-    /****************************************
-    파이프 부분을 일반 콜라이더, 포인트 획득(사이) 부분을 isTrigger로 구분해두었지만,
-    플레이어 코드 상에서 태그로 구별하신 듯해 주석 처리해둡니다.
-
-    
-    //private void OnCollisionEnter2D(Collision2D col)
-    //{
-    //    if (col.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
-    //    {
-    //        //플레이어 사망 처리 및 게임 종료 호출
-    //    }
-    //}
-    //
-    //private void OnTriggerEnter2D(Collider2D col)
-    //{
-    //    if (col.CompareTag("Player"))
-    //    {
-    //        //점수 증가
-    //        //게임매니저 함수 호출 or 이곳에서 카운트한 뒤 게임매니저로 전송
-    //    }
-    //}
-     ****************************************/
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        //이 부분은 유니티에서 오류나면 CompareTag로 변경
+        if (col.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+            player.Die();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Score++;
+        }
+    }
 }
